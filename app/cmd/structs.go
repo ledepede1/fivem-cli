2af14d3b8942654ca9aa34de9dbb3aad3321ea52
Mikkel <mikkelhulstroem@gmail.com>
2024-01-13 20:49:14 +0100

    Added: Now the structcreate command is functional

--- app/cmd/structs.go
+++ app/cmd/structs.go
@@ -1,21 +1,34 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/ledepede1/forgestruct-cli/pkg/structures"
 	"github.com/spf13/cobra"
 )
 
+var Cname string
+var label string
+var url string
+
 var editConf = &cobra.Command{
-	Use:   "structs",
-	Short: "Want to add new structures or edit existing structures?",
-	Long: `The structs command is used for editing and adding structures
+	Use:   "structcreate",
+	Short: "Want to add a new structures?",
+	Long: `The structs command is used for adding structures
 to the config.json`,
 	Run: func(cmd *cobra.Command, args []string) {
-		structures.CreateStruct("test3", "Test 3", "https://github.com/ledepede1/default-structure")
+		if len(Cname) == 0 || len(label) == 0 || len(url) == 0 {
+			fmt.Println("Wrong usage of command needs to be `fstruct --name {name} --label {label} --url {url}`")
+		} else {
+			structures.CreateStruct(Cname, label, url)
+		}
 	},
 }
 
 func init() {
-	cobra.OnlyValidArgs(editConf, []string{"edit", "add"})
+	editConf.Flags().StringVarP(&Cname, "name", "n", "", "Name that will be used in the command")
+	editConf.Flags().StringVarP(&label, "label", "l", "", "Label of the command")
+	editConf.Flags().StringVarP(&url, "url", "u", "", "Url/Link to the github repo")
+
 	rootCmd.AddCommand(editConf)
 }
